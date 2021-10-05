8c39e960922fbfe418c1a3df242386c754a35c0d
rafael <rafael.kuchla.flores@gmail.com>
2021-10-04 21:25:36 -0300

    Colocando "await" nos future

--- lib/database/app_database.dart
+++ lib/database/app_database.dart
@@ -2,8 +2,24 @@ import 'package:bitebank/models/contato.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
+Future<Database> getDatabase() async {
+  final String path = join(await getDatabasesPath(), 'bytebank.db');
+
+  return openDatabase(
+    path,
+    onCreate: (db, version) {
+      db.execute('CREATE TABLE contatos('
+          'id INTEGER PRIMARY KEY,'
+          'nome TEXT,'
+          'numero_conta INTEGER)');
+    },
+    version: 1,
+    //    onDowngrade: onDatabaseDowngradeDelete,
+  );
+}
+
 //Função para criar banco
-Future<Database> createDatabase() {
+/*Future<Database> createDatabase() {
   return getDatabasesPath().then((dbPath) {
     //join=função que junta diversas strings que serão enviadas como argumento.
     final String path = join(dbPath, 'bytebank.db');
@@ -16,21 +32,37 @@ Future<Database> createDatabase() {
     //    onDowngrade: onDatabaseDowngradeDelete,
     );
   });
+}*/
+
+Future<int> salvar(Contato contato) async {
+  final Database db = await getDatabase();
+
+  final Map<String, dynamic> contatoMap = Map();
+  //contatoMap['id'] = contato.id;
+  contatoMap['nome'] = contato.nome;
+  contatoMap['numero_conta'] = contato.numero_conta;
+
+  return db.insert('contatos', contatoMap);
+
 }
 
-Future<int> salvar(Contato contato) {
-  return createDatabase().then((db) {
-    final Map<String, dynamic> contatoMap = Map();
-    //contatoMap['id'] = contato.id;
-    contatoMap['nome'] = contato.nome;
-    contatoMap['numero_conta'] = contato.numero_conta;
+Future<List<Contato>> buscar() async{
+  final Database db = await getDatabase();
+  final List<Contato> contatos = [];
 
-    return db.insert('contatos', contatoMap);
-  });
+  for (Map<String, dynamic> linha in await db.query('contatos')) {
+    final Contato contato = Contato(
+      linha['id'],
+      linha['nome'],
+      linha['numero_conta'],
+    );
+    contatos.add(contato);
+  }
+  return contatos;
 }
 
-Future<List<Contato>> buscar() {
-  return createDatabase().then((db) {
+/*Future<List<Contato>> buscar() {
+  return getDatabase().then((db) {
     return db.query('contatos').then((maps) {
       final List<Contato> contatos = [];
       for (Map<String, dynamic> map in maps) {
@@ -44,4 +76,4 @@ Future<List<Contato>> buscar() {
       return contatos;
     });
   });
-}
+}*/
